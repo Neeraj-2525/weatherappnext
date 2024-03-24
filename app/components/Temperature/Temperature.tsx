e1744e6bdfe40b4224689c622abd9938fdd24269
Neeraj-2525 <neerajkumara2525@gmail.com>
2024-03-24 11:57:14 +0530

    temperature component completed

--- app/components/Temperature/Temperature.tsx
+++ app/components/Temperature/Temperature.tsx
@@ -1,32 +1,33 @@
 "use client"
 
 import { useGlobalContext } from '@/app/context/globalContext'
-import { clearSky, cloudy, drizzleIcon, rain, snow, thunderstorm } from '@/app/utils/icons';
+import { clearSky, cloudy, drizzleIcon, navigation, rain, snow, thunderstorm } from '@/app/utils/icons';
 import { kelvinToCelcius } from '@/app/utils/misc';
-import React from 'react'
+import moment from 'moment';
+import React, { useEffect } from 'react'
 
 const Temperature = () => {
     const { forecast } = useGlobalContext();
     const { main, timezone, name, weather } = forecast;
 
-    // console.log(kelvinToCelcius(main.temp));
+    console.log(timezone);
 
     if (!forecast || !weather)
         return <div>Loading...</div>
 
     const temp = kelvinToCelcius(main?.temp);
-    const minTemp = kelvinToCelcius(main?.minTemp);
-    const maxTemp = kelvinToCelcius(main?.maxTemp);
-    
+    const minTemp = kelvinToCelcius(main?.temp_min);
+    const maxTemp = kelvinToCelcius(main?.temp_max);
+
 
     //state
     const [localTime, setLocalTime] = React.useState<string>("");
     const [currentDay, setCurrentDay] = React.useState<string>("");
 
-    const {main: weatherMain, description} = weather[0];
+    const { main: weatherMain, description } = weather[0];
 
-    const getIcon = ()=>{
-        switch(weatherMain){
+    const getIcon = () => {
+        switch (weatherMain) {
             case "Drizzle":
                 return drizzleIcon;
             case "Rain":
@@ -44,12 +45,43 @@ const Temperature = () => {
         }
     }
 
+    // live time update
+    useEffect(()=>{
+        // update time every second
+        const interval = setInterval(()=>{
+            const localMoment = moment().utcOffset(timezone/60);
+            // set custom format 12 hour format
+            const formatedTime = localMoment.format("HH:mm a");
+            // day of the week
+            const day = localMoment.format("dddd");
+
+            setLocalTime(formatedTime);
+            setCurrentDay(day);
+        },1000)
+    },[])
+
     return (
-        <div className='pt-6 pb-5 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none'>
+        <div className='pt-6 pb-5 px-4 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none'>
             <p className="flex justify-between items-center">
                 <span className="font-medium">{currentDay}</span>
                 <span className="font-medium">{localTime}</span>
             </p>
+            <p className='pt-2 font-bold flex gap-1 items-center'>
+                <span>{name}</span>
+                <span>{navigation}</span>
+            </p>
+
+            <p className='py-10 text-8xl font-bold self-center'>{temp}°</p>
+            <div>
+                <div>
+                    <span>{getIcon()}</span>
+                    <p className='pt-2 capitalize text-lg font-medium'>{description}</p>
+                </div>
+                <p className='flex items-center gap-2'>
+                    <span>Low: <strong>{minTemp}</strong>°</span>
+                    <span>High: <strong>{maxTemp}</strong>°</span>
+                </p>
+            </div>
         </div>
     )
 }
