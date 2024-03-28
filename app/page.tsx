20a2e01f42dbca9458f3fd53c9fc0800dc0c2f49
Neeraj-2525 <neerajkumara2525@gmail.com>
2024-03-28 17:46:25 +0530

    un-commented components in home page

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
 
         <div className="pb-4 flex flex-col gap-4 md:flex-row">
           <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
-            {/* <Temperature /> */}
-            {/* <FiveDayForecast /> */}
+            <Temperature />
+            <FiveDayForecast />
           </div>
 
           <div className="flex flex-col w-full">
             <div className="instruments grid  gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
-              {/* <AirPollution />
+              <AirPollution />
               <Sunset />
               <Wind />
               <DailyFocus />
@@ -44,11 +44,11 @@ export default function Home() {
               <FeelsLike />
               <Humidity />
               <Visibility />
-              <Pressure /> */}
+              <Pressure />
             </div>
             <div className="mapbox-container mt-4 flex gap-4">
-              {/* <Mapbox /> */}
-              {/* <TopCities /> */}
+              <Mapbox />
+              <TopCities />
             </div>
           </div>
         </div>
